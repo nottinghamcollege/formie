18bf40eae73eb218557e45c6c3ddd067db6e7539
Josh Crawford <joshua@sgroup.com.au>
2024-11-14 11:00:19 +1100

    Fix user permissions for form access in the control panel

--- src/elements/Form.php
+++ src/elements/Form.php
@@ -2104,7 +2104,7 @@ class Form extends Element
 
         // Check if the user has permission to edit this form
         if ($userSession && !$userSession->checkPermission('formie-manageForms')) {
-            if (!$userSession->checkPermission('formie-manageForm:' . $this->uid)) {
+            if (!$userSession->checkPermission('formie-manageForms:' . $this->uid)) {
                 return null;
             }
         }
